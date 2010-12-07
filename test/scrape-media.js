28bfc8c5f8bf06b8b4d719c03e0d29951cfa6c74
AJ ONeal <coolaj86@gmail.com>
2010-12-07 04:07:46 +0000

    updated scrape-media with count and full paths

--- test/scrape-media.js
+++ test/scrape-media.js
@@ -1,18 +1,28 @@
 (function () {
   var walk = require('walk'),
-    mediatags = require('../lib/mediatags');
+    util = require('util'),
+    mediatags = require('../lib/mediatags'),
+    count = 0;
 
   walk("/mnt/data").whenever(function (err, path, nodes, sorted) {
     //console.log(path);
-    sorted.files.forEach(function (file) {
+    if (err) {
+      util.debug(err);
+    }
+    if (!sorted) {
+      return;
+    }
+    (sorted.files||[]).forEach(function (file) {
       //console.log(file);
       mediatags(path + '/' + file.name).when(function (err, data) {
+        count += 1;
         if (err) {
-          console.log('error parsing output for ' + [path,file.name].join('/'));
-          //console.log(err);
+          util.debug('error parsing output for ' + [path,file.name].join('/'));
+          util.debug(err);
           return;
         }
-        console.log(data);
+        console.log('['+count+'] ' + path + '/' + file.name);
+        //console.log(data);
       });
     });
   });
